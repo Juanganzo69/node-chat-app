6f54216c90661045e166377d765fff598c7ceaeb
Juanganzo69 <juan1910@live.com.mx>
2017-07-19 00:43:16 -0500

    connect y disconnect socketIO

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http  = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 var port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
@@ -7,8 +9,21 @@ const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, ()  => {
+io.on('connection', (socket) => {
+    console.log('usuario conectado');
+
+    socket.on('disconnect', () => {
+        console.log('usuario desconectado');
+    });
+});
+
+
+
+server.listen(port, ()  => {
     console.log(`El servidor está escuchando en el puerto ${ port }`);
 });

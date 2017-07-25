3e520635a7799baa26203bcb5dc7a684a0ced4dd
Juanganzo69 <juan1910@live.com.mx>
2017-07-25 02:26:04 -0500

    Corregido bug de los rooms

--- public/js/room.js
+++ public/js/room.js
@@ -2,20 +2,21 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('conectado...');
-    // socket.emit('sendRooms',{ k: null } ,function(k){
-    //     console.log(k);
-    // });
+    socket.emit('sendRooms',{ k: null } ,function(k){
+        var ol = jQuery('<ol></ol>');
+
+        k.forEach( function(room){
+            ol.append(jQuery('<li></li>').text(room));
+        });
+
+        jQuery('#listRooms').html(ol);
+    });
 });
 
 
 socket.on('room', function(rooms){
         console.log(rooms);
-        // var template = jQuery('#rooms').html();
 
-        // var html = Mustache.render(template,{
-        //     rooms: rooms
-        // });
-        // jQuery('#listRooms').append(html);
         var ol = jQuery('<ol></ol>');
 
         rooms.forEach( function(room){
